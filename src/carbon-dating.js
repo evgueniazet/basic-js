bec833bb0650b1a0449306e0347322877bc1408a
evgueniazet <zelenkoea19@gmail.com>
2022-10-15 20:26:22 +0300

    add : carbon dating solved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,10 +17,46 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function dateSample(sampleActivity) {
+
+  let age = 0;
+  if (sampleActivity === undefined) {
+    console.log('sampleActivity', sampleActivity);
+    return false;
+  } else {
+    if (typeof sampleActivity != 'string') {
+      console.log('sampleActivity1', sampleActivity);
+      return false;
+    } else {
+      if (isNaN(sampleActivity)) {
+        console.log('sampleActivity2', sampleActivity);
+        return false;        
+      } else {
+        if (Number(sampleActivity) === 0) {
+          console.log('ll', sampleActivity);
+          return false; 
+        }
+
+        if (sampleActivity > 15) {
+          console.log('sampleActivity3', sampleActivity);
+          return false;
+        } else {
+          let k = Math.log(2) / HALF_LIFE_PERIOD;
+          let x = MODERN_ACTIVITY / sampleActivity;
+          t = Math.log(x) / k;
+          let age = Math.ceil(t);
+          console.log('k', age);
+        }
+      }
+    }
+  }
+
+  return age;
+};
+dateSample('1');
+
+
+
 
 module.exports = {
   dateSample
