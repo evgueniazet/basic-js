131000781a63899321952a5568f69ad9a9af5ccd
evgueniazet <zelenkoea19@gmail.com>
2022-10-16 01:15:24 +0300

    add: carbon dating and dream team solved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,34 +18,29 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
+  let age = false;
 
-  let age = 0;
   if (sampleActivity === undefined) {
-    console.log('sampleActivity', sampleActivity);
     return false;
   } else {
     if (typeof sampleActivity != 'string') {
-      console.log('sampleActivity1', sampleActivity);
       return false;
     } else {
       if (isNaN(sampleActivity)) {
-        console.log('sampleActivity2', sampleActivity);
         return false;        
       } else {
         if (Number(sampleActivity) === 0) {
-          console.log('ll', sampleActivity);
           return false; 
         }
 
-        if (sampleActivity > 15) {
-          console.log('sampleActivity3', sampleActivity);
+        if (sampleActivity > 15 || sampleActivity < 0) {
           return false;
         } else {
           let k = Math.log(2) / HALF_LIFE_PERIOD;
           let x = MODERN_ACTIVITY / sampleActivity;
+
           t = Math.log(x) / k;
-          let age = Math.ceil(t);
-          console.log('k', age);
+          age = Math.ceil(t);
         }
       }
     }
@@ -53,10 +48,6 @@ function dateSample(sampleActivity) {
 
   return age;
 };
-dateSample('1');
-
-
-
 
 module.exports = {
   dateSample
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
@@ -13,10 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+
+// let members = ['Matt', 'Ann', 'Dmitry', 'Max'];
+// let members = ['Ol   via', 1111, 'Lily', 'oscar', true, null, 'volodia'];
+// let members = [
+//   ['David Abram'],
+//   ['Robin Attfield'],
+//   'Thomas Berry',
+//   ['Paul R.Ehrlich'],
+//   'donna Haraway',
+//   ' BrIaN_gOodWiN  ',
+//   {
+//     0: 'Serenella Iovino'
+//   },
+//   'Erazim Kohak',
+//   '  val_plumwood',
+// ]
+
+function createDreamTeam(members) {
+  let nameDreamTeam = '';
+
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        let name = members[i].replaceAll(' ', '');
+        let firstLetter = name.charAt(0);
+
+        nameDreamTeam += firstLetter;
+      }
+    };
+  } else {
+    return false;
+  }
+
+  let nameDreamTeamArray = nameDreamTeam.toUpperCase().split('');
+  nameDreamTeamArray.sort();
+
+  return nameDreamTeamArray.join('');
+};
 
 module.exports = {
   createDreamTeam
